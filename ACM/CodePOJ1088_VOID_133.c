5c6ee908d939de4d75823108f630c2a4e3b6d856
VOID_133 <zhangjianqiu_133@yeah.net>
2014-04-19 16:19:51 +0800

    自己重新写了一次 ，，希望能找到更简单的方法

--- ACM/CodePOJ1088_VOID_133.c
+++ ACM/CodePOJ1088_VOID_133.c
@@ -2,34 +2,30 @@
 #include<string.h>
 
 const int MAX_N=105;
-int dp_f(int r,int c);
-int data [MAX_N][MAX_N];
 int dp[MAX_N][MAX_N];
+int data[MAX_N][MAX_N];
+int R,C;
 int dx[4]={1,0,-1,0};
 int dy[4]={0,1,0,-1};
-int R,C;
-
+int dp_f(int r,int c);
 int main(void)
 {
-    int maxn=0;
-    memset(dp,0,sizeof(dp));
-    scanf("%d%d",&R,&C);
+    memset(dp,0,sizeof(dp));            //初始化
+    scanf("%d%d",&R,&C);                //接收输入
     for(int i=0;i<R;i++)
         for(int j=0;j<C;j++)
             scanf("%d",&data[i][j]);
-
-    for(int i=0;i<R;i++)
-        for(int j=0;j<R;j++)
+    for(int i=0;i<R;i++)                //对每个格子进行记忆化搜索
+        for(int j=0;j<C;j++)
             dp_f(i,j);
-    for(int i=0;i<R;i++)
-            //printf("\n");
-        for(int j=0;j<R;j++)
-        if(maxn<dp[i][j]) maxn=dp[i][j];
-            //{printf("%d ",dp[i][j]);}
+    int maxn=0;
+    for(int i=0;i<R;i++)                //取最大值
+        for(int j=0;j<C;j++)
+            if(maxn<dp[i][j]) maxn=dp[i][j];
     printf("%d\n",maxn+1);
+    return 0;
 
 }
-
 
 int dp_f(int r,int c)
 {
@@ -37,14 +33,14 @@ int dp_f(int r,int c)
     else
     {
         for(int i=0;i<4;i++)
-            if(r+dy[i]>=0 && r+dy[i]<R && c+dx[i]>=0 && c+dx[i]<C)
+        {
+            if(r+dy[i]<R && r+dy[i]>=0 && c+dx[i]>=0 && c+dx[i]<C)
             {
                 if(data[r][c]>data[r+dy[i]][c+dx[i]])
-                {
                     if(dp[r][c]<dp_f(r+dy[i],c+dx[i])+1)
                         dp[r][c]=dp_f(r+dy[i],c+dx[i])+1;
-                }
             }
+        }
+        return dp[r][c];
     }
-    return dp[r][c];
 }
