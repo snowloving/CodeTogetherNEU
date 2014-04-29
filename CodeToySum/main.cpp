b4deed0f7aa8c83d7b8e68358d9f18bbaeb4e1c4
VOID001 <zhangjianqiu_133@yeah.net>
2014-04-29 21:47:39 +0800

    Change the input method

--- CodeToySum/main.cpp
+++ CodeToySum/main.cpp
@@ -12,73 +12,75 @@ int main (void)
 	int N;
 	int sel;
 	int sum=0;
-	scanf("%d",&N);
-	memset (vis,0,sizeof(vis));
-	for(int i=0;i<N;i++)
+	while(scanf("%d",&N)!=EOF)
 	{
-		scanf("%d",&sel);
-		vis[sel]=1;
-		sum+=(sel-1);
-	}
-	int tmpsum=0;
-	int cnt=0;
-	int pta=1,ptb=s;
-	while(pta<ptb)
-	{
-		if(vis[pta])
+		memset (vis,0,sizeof(vis));
+		for(int i=0;i<N;i++)
 		{
-			if(!vis[ptb])
+			scanf("%d",&sel);
+			vis[sel]=1;
+			sum+=(sel-1);
+		}
+		int tmpsum=0;
+		int cnt=0;
+		int pta=1,ptb=s;
+		while(pta<ptb)
+		{
+			if(vis[pta])
 			{
-				if(cnt!=0) printf(" ");
-				printf("%d",ptb);
-				vis[ptb]=1;
-				cnt++;
+				if(!vis[ptb])
+				{
+					if(cnt!=0) printf(" ");
+					printf("%d",ptb);
+					vis[ptb]=1;
+					cnt++;
+				}
+				else if(vis[ptb])
+				{
+					for(int i=1;i<=s;i++)
+					{
+						if((vis[i] && vis[s+1-i])==0)
+						{
+							vis[i]=vis[s+1-i]=1;
+							if(cnt!=0) printf(" ");
+							printf("%d",i);
+							cnt++;
+							printf(" %d",s+1-i);
+							cnt++;
+							break;
+						}
+					}
+				}
 			}
 			else if(vis[ptb])
 			{
-				for(int i=1;i<=s;i++)
+				if(!vis[pta])
 				{
-					if((vis[i] && vis[s+1-i])==0)
+					if(cnt!=0) printf(" ");
+					printf("%d",pta);
+					vis[pta]=1;
+					cnt++;
+				}
+				else if(vis[pta])
+				{
+					for(int i=1;i<=s;i++)
 					{
-						vis[i]=vis[s+1-i]=1;
-						if(cnt!=0) printf(" ");
-						printf("%d",i);
-						cnt++;
-						printf(" %d",s+1-i);
-						cnt++;
-						break;
+						if((vis[i] && vis[s+1-i])==0)
+						{
+							vis[i]=vis[s+1-i]=1;
+							if(cnt!=0) printf(" ");
+							printf("%d",i);
+							cnt++;
+							printf(" %d",s+1-i);
+							cnt++;
+							break;
+						}
 					}
 				}
 			}
+			if(cnt==N) break;
+			pta++;
+			ptb--;
 		}
-		else if(vis[ptb])
-		{
-			if(!vis[pta])
-			{
-				if(cnt!=0) printf(" ");
-				printf("%d",pta);
-				vis[pta]=1;
-				cnt++;
-			}
-			else if(vis[pta])
-			{
-				for(int i=1;i<=s;i++)
-				{
-					if((vis[i] && vis[s+1-i])==0)
-					{
-						vis[i]=vis[s+1-i]=1;
-						if(cnt!=0) printf(" ");
-						printf("%d",i);
-						cnt++;
-						printf(" %d",s+1-i);
-						cnt++;
-						break;
-					}
-				}
-			}
-		}
-		if(cnt==N) break;
-		pta++;
-		ptb--;
 	}
 }
