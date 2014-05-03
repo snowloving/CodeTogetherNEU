bcdd17a6fecf2b8ff9fe4a35e842a2ebe2e62cb2
VOID001 <zhangjianqiu_133@yeah.net>
2014-05-03 10:38:21 +0800

    I think ther is some problem with the mark ,and the algorithm is correct

--- CodeMaxSum/main.cpp
+++ CodeMaxSum/main.cpp
@@ -17,26 +17,46 @@ int main(void)
 	int N;
 	int dp[MAX];
 	int s[MAX];
+	int st[MAX];
 	scanf("%d",&N);
 	for(int i=1;i<=N;i++)
 	{
 		memset(dp,0,sizeof(dp));		//所有数都是负的怎么办
+		memset(st,0,sizeof(st));
 		int n;
+		int maxn=-INF;
+		int pst,ped;
 		scanf("%d",&n);
 		for(int oo=1;oo<=n;oo++)
 		{
 			scanf("%d",&s[oo]);
 		}
-		for(int oo=1;oo<=n;oo++)
+		dp[1]=s[1];
+		int mark=1;;
+		for(int oo=pst;oo<=n;oo++)
 		{
 			if(dp[oo-1]<0)
+			{
 				dp[oo]=s[oo];
+				st[oo]=oo;
+				mark=oo;
+			}
 			else
-				dp[oo]=max(dp[oo-1],dp[oo-1]+s[oo]);
+			{
+				dp[oo]=dp[oo-1]+s[oo];
+				st[oo]=st[mark];
+			}
 		}
-		for(int j=1;j<=n;j++) printf("%d ",dp[j]);
-		printf("\n");
-		printf("Case %d:\n%d",i,dp[n]);
+		for(int oo=1;oo<=n;oo++)
+		{
+			if(maxn<dp[oo])
+			{
+				maxn=dp[oo];
+				ped=oo;
+				pst=st[oo];
+			}
+		}
+		printf("Case %d:\n%d %d %d\n\n",i,maxn,!pst?1:pst,ped);
 	}
 	return 0;
 }
